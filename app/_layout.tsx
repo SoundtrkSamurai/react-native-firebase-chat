806c654c92a1e3aac82cb6bd8118edabbb85e7a5
SoundtrkSamurai <furtimat3@yahoo.com>
2024-07-24 22:06:33 -0700

    main: first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+import "../global.css";
+import React from "react";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
